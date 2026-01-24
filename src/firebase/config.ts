bda15fad67dd582e912d7a4d66c949a34a58b8c4
Andrew Sardone <andrew@autoknerd.com>
2026-01-24 20:53:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8028797920-12261",
+  "appId": "1:919724092762:web:7869bce7f4b8ec7dab1763",
+  "apiKey": "AIzaSyCTLbPGP5v2Vu4ahy0XWStVkCUWnYtRxdA",
+  "authDomain": "studio-8028797920-12261.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "919724092762"
+};
