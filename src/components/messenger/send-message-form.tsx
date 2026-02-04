9ac452a6a9e00281e116464b881c121a70d63a57
Andrew Sardone <andrew@autoknerd.com>
2026-02-04 23:11:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/messenger/send-message-form.tsx
+++ src/components/messenger/send-message-form.tsx
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { User, Dealership, UserRole } from '@/lib/definitions';
-import { sendMessage, getTeamMemberRoles } from '@/lib/data';
+import { sendMessage, getTeamMemberRoles } from '@/lib/data.client';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
